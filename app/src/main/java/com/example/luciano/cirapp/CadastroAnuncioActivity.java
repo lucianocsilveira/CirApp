6669f5ac7567e1592b41c0d8767aac2202d1b286
Diego Miguel <dimiiguel@gmail.com>
2017-03-17 17:23:18 -0300

    Tratamento dos Spinners do Cadastro de Anúncio

--- app/src/main/java/com/example/luciano/cirapp/CadastroAnuncioActivity.java
+++ app/src/main/java/com/example/luciano/cirapp/CadastroAnuncioActivity.java
@@ -2,12 +2,38 @@ package com.example.luciano.cirapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class CadastroAnuncioActivity extends AppCompatActivity {
+public class CadastroAnuncioActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner spinCategoria;
+    String[] categoriaList = {"Categoria", "Plástico", "Vidro", "Borracha", "Papel", "Óleo", "Eletrônicos"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_anuncio);
+
+        spinCategoria = (Spinner) findViewById(R.id.spinCategoria);
+
+        ArrayAdapter<String> spinCategAdapter = new ArrayAdapter<String>(this,
+                                                    R.layout.support_simple_spinner_dropdown_item,
+                                                    categoriaList);
+
+        spinCategoria.setAdapter(spinCategAdapter);
+        spinCategoria.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
